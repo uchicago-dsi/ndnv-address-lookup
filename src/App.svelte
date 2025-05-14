c22a81bfc4c801a06f92a7aac4189b374ad0aa7d
Jim Pivarski <jpivarski@gmail.com>
2025-05-14 10:57:54 -0500

    basic sattelite map in MapLibre GL

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,26 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte'
+  import { Map } from 'maplibre-gl';
+  import 'maplibre-gl/dist/maplibre-gl.css';
+
+  let map;
+  let mapContainer;
+
+  onMount(() => {
+    map = new Map({
+      container: mapContainer,
+      style: "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json",
+      bounds: [-104.5181265794389, 45.63232713888373, -96.06887947161051, 49.2702273475217],
+    });
+  });
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte; hello!</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .map {
+    width: 100%;
+    height: 100%;
   }
 </style>
+
+<div class="map" bind:this={mapContainer}></div>
