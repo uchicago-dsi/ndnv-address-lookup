1b7e0b77bc3ea5cb9ada19e2febbb103b4a2dbac
Jim Pivarski <jpivarski@gmail.com>
2025-05-14 13:05:47 -0500

    developing a personal style

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   onMount(() => {
     map = new Map({
       container: mapContainer,
-      style: "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json",
+      style: "/map-style.json",
       bounds: [-104.5181265794389, 45.63232713888373, -96.06887947161051, 49.2702273475217],
     });
   });
